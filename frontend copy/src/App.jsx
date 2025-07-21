4317f3d945e380b332d90f767212e0f22a54b2ad
Perla Ruiz <perla.ruizz04@gmail.com>
2025-07-20 23:56:08 -0400

    added new files

---
+++ frontend copy/src/App.jsx
@@ -0,0 +1,29 @@
+import Navbar from './components/navbar/navbar'
+import Profile from './components/profile/profile'
+import Projects from './components/projects/projects'
+import Experiences from './components/experiences/experiences'
+import Chat from './components/chat/chat'
+
+import './App.css'
+
+    function App() {
+        return (
+        <>
+            <Navbar/>
+            <Profile/>
+            <Chat/>
+            <div id="projects">
+              <h2>Projects</h2>
+              <Projects name="Project 1" description="Project 1 Description" github="https://github.com"></Projects>
+              <Projects name="Project 2" description="Proejct 2 Description" github="https://github.com"></Projects>
+            </div>
+            <div id ="exp">
+            <h2>Experiences</h2>
+              <Experiences title="Experience 1" info="Experience 1 Description" dates="Date 1 - Date 2" bullets={['']}></Experiences>
+              <Experiences title="Experience 2" info="Experience 2 Description" dates="Date 1 - Date 2" bullets={['']}></Experiences>
+            </div>
+          </>
+        )
+    }
+
+    export default App
---
+++ frontend copy/src/components/chat/chat.jsx
@@ -0,0 +1,48 @@
+import {useState } from 'react'
+import './chat.css'
+
+function Chat() {
+    const [ messeges, setMesseges] = useState([])
+    const [ userInput, setUserInput] = useState('')
+
+    async function getResponse(){
+        try{
+            if(!userinput) return 
+            const response = await fetch('http://localhost:4000/chat', {
+                method: 'POST',
+                headers : {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ userInput})
+            }) 
+            if (!response.ok){
+                throw new Error('oops, something went wrong')
+            }
+            const {message} = await response.json()
+            setMesseges([...messeges, userInput, message])
+        } catch(error){
+            console.log(error)
+            return 'Someting went wrong!'
+        }
+    }
+    return (
+        <div id = "chat">
+            <form onSubmit = {(e) => e.preventDefault ()}>
+                <h2>Ask a question</h2>
+                <input type = "text" name = "user-input" id="questionInput" 
+                    placeholder= "what would you like to ask" 
+                    onChange = {e => setUserInput(e.target.value)}/>
+                <button type = "submit" onClick={getResponse} >Submit</button>
+            </form>
+            {
+                messeges && messeges.map((text, index)=> (
+                    <div key={index} className = "chatbox">
+                        <p className={index % 2 == 0 ? "user-message" : "chatbot-response"}>{text}</p>
+                        </div>
+                ))
+            }
+        </div>
+        
+    )
+}
+export default Chat
