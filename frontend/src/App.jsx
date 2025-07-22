00f1661e1ba46f3b44d7bc7565b5efd043f11b54
Perla Ruiz <perla.ruizz04@gmail.com>
2025-07-22 19:08:36 -0400

    Implemented a contact me section below each page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Projects from './components/projects/projects'
 import Experiences from './components/experiences/experiences'
 import Classes from './components/classes/classes';
 import Chat from './components/chat/chat'
+import Contact from './components/contact/Contact';
 
 import './App.css'
 
@@ -28,16 +29,22 @@ import './App.css'
             <>
         <h2>Experiences</h2>
         <Experiences
-            title="Experience 1"
-            info="Experience 1 Description"
-            dates="Date 1 - Date 2"
-            bullets={['Worked on frontend', 'Collaborated with team']}
+            title="BostonHacks"
+            info="Marketing Organizer"
+            dates="Jan 2025 - Current"
+            bullets={['Manage social media campaigns and promotional content', 'Collaborate with team','Build partnerships with on and off-campus organizations and tech clubs', 'Create unique and creative content']}
             />
         <Experiences
-            title="Experience 2"
-            info="Experience 2 Description"
-            dates="Date 3 - Date 4"
-            bullets={['Built backend API', 'Managed deployment']}
+            title="Seminole Tribe of Florida"
+            info="I.T. Department Summer Internship"
+            dates="Jun 2024 - Aug 2024"
+            bullets={['Optimized queries in ServiceNow', 'Collaborated with IT staff across hardware, software, and network teams','Observed departmental operations including system upgrades, asset management, and customer service']}
+            />
+        <Experiences
+            title="Tele-fund at Boston University "
+            info="Caller"
+            dates="Jan 2024 - May 2024"
+            bullets={['Engaged with Boston University alumni to help raise fundrasing', 'Managed Fostered meaningful conversations with diverse individuals','Worked in fast-paced, high-volume calling environment while maintaing professionalism and demonstrating adaptability']}
             />
             </>
         }
@@ -51,7 +58,7 @@ import './App.css'
             <Projects
                 name="Personal Portfolio"
                 description="personal website built from scratch using react,css, jsx"
-                github="https://github.com"
+                github="https://github.com/perlards/website"
             />
             <Projects
                 name="Project 2"
@@ -67,6 +74,7 @@ import './App.css'
 
       {/* Chat appears at the bottom of every page */}
     {(location.pathname === '/' || location.pathname === '/chat') && <Chat />}
+    
     </>
 )
 }
---
+++ frontend/src/components/contact/contact.jsx
@@ -0,0 +1,17 @@
+import './contact.css';
+
+function Contact() {
+return (
+    <footer className="contact">
+    <h2>Contact Me!</h2>
+    <p>Email: perla.ruizz04@gmail.com</p>
+    <p>Phone: (954) 945-0820</p>
+    <p>LinkedIn: <a href="https://www.linkedin.com/in/perla-ruiz-3a546b328/" target="_blank" rel="noopener noreferrer">yourprofile</a></p>
+    <p>GitHub: <a href="https://github.com/perlards" target="_blank" rel="noopener noreferrer">yourprofile</a></p>
+    <p>Resume: <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">yourprofile</a></p>
+
+    </footer>
+);
+}
+
+export default Contact;
