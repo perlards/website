b2723addaec7dd626afa195fa884cbafdccccde8
Perla Ruiz <perla.ruizz04@gmail.com>
2025-07-27 18:25:53 -0400

    Improve accessibility: keyboard navigation, alt text, and ARIA live updates on chat, checked contrast ratio

--- frontend/src/components/profile/profile.jsx
+++ frontend/src/components/profile/profile.jsx
@@ -8,15 +8,14 @@ import Resume from '../../assets/resume.png'
 function Profile() {
     return (
         <div id="profile">
-            <div><img width="250" src={ProfilePic} alt="Profile" className="profile-pic" /></div>
+            <div><img width="250" src={ProfilePic} alt="Profile Picture" className="profile-pic" /></div>
             <div id="profile-text">
                 <h2>Perla Ruiz del Solar</h2>
                 <p>Boston University Student</p>
                 <div className="icons">
-                    <a target="_blank" href="https://www.linkedin.com/in/perla-ruiz-3a546b328//"><img id="linkedin-img" width="60" src={LinkedinImage} /></a>
-                    <a target="_blank" href="https://github.com/perlards"><img id="github-img" width="60" src={GitHubIcon} /></a>
-                    <a target="_blank" rel="noopener noreferrer" href="/resume.pdf">
-                        <img id="resume-img" width="60" src={Resume} alt="Resume Icon" />
+                    <a target="_blank" href="https://www.linkedin.com/in/perla-ruiz-3a546b328//"><img id="linkedin-img" width="60" src={LinkedinImage} alt="LinkedIn Icon"  /></a>
+                    <a target="_blank" href="https://github.com/perlards"><img id="github-img" width="60" src={GitHubIcon} alt="Github Icon"  /></a>
+                    <a target="_blank" rel="noopener noreferrer" href="/resume.pdf"> <img id="resume-img" width="60" src={Resume} alt="Resume Icon" />
                     </a>
                 </div>
             </div>
